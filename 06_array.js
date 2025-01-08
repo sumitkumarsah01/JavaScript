3f7c5071590c1903d5e734f08eca6d36f79bf7bc
sumit kumar sah <sumitkumar92365@gmail.com>
2025-01-08 23:24:29 +0530

    DAY 4

---
+++ 06_array.js
@@ -0,0 +1,78 @@
+
+// array
+
+const myArr=[0,1,2,3,4,5];
+const myHeros=["shakiman","naagraj"];
+console.log(myArr[1]);
+
+const myArr2=new Array(1,2,3,4);
+
+
+// array method
+//push     add the value in array last possition
+myArr.push(6);
+myArr.push(7);
+console.log(myArr)
+
+//pop   delet the value in arrray last possition
+
+myArr.pop();
+console.log(myArr)
+
+//unshift   add the value in starting possition
+// shift  delet the value in starting possition
+
+
+//console.log(myArr.indexOf(9)); // because index 9 exit hi nhi krta hai
+
+const newArr=myArr.join();
+//console.log(myArr);
+//console.log(newArr) // iska type ab string ho gya hai 
+
+
+// slice , splice (important)
+
+
+//slice
+
+//console.log(myArr.slice(1,3))//   index value pass krte hai aur last include nhi hota hai aur ye 
+//console.log(myArr); // original array me change nhi lata hai
+
+//splice
+
+
+const newArr2=myArr.splice(1,3)
+console.log(newArr2); // isme ye bhi different hai ki last wala value include rahta hai but main difference hai ki ye orignal array me se remove kr deta hai 
+
+console.log(myArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07_array_part2.js
@@ -0,0 +1,29 @@
+const marvel_heros=["thor","Ironman","spiderman"];
+const dc_heros=["superman","flash","batman"];
+
+//marvel_heros.push(dc_heros);
+//console.log(marvel_heros);  // array me array isliye dal de rha hai ki kyuki javaScript me array me different type ke element ik hi array me aa sakte hai is second array ko ik element smj raha hai
+
+// concat 
+// isme new array banta hai aur isko new varible me store krwana padta hai 
+
+const new_marve_heros=marvel_heros.concat(dc_heros);
+//console.log(new_marve_heros); // ye do array ko add kr deta hai
+
+// spread operator
+
+const newmarvelsheros=[...marvel_heros,...dc_heros];
+//console.log(newmarvelsheros);
+
+
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]];
+const real_another_array=another_array.flat(Infinity);
+console.log(real_another_array);
+
+
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh"));
+
+
+
+
