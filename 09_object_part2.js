cb9d324a7bc9989bfc5245b72c4481c928a37ac4
sumit kumar sah <sumitkumar92365@gmail.com>
2025-01-11 10:29:23 +0530

    DAY 5

---
+++ 09_object_part2.js
@@ -0,0 +1,49 @@
+// const tinderUser=new Object(); this is singleton
+
+const tinderUser={};
+ tinderUser.id="727s;d"
+ tinderUser.name="name";
+ tinderUser.isLoggedIn=false;
+
+//console.log(tinderUser)
+
+const regularUser={
+    email:"sumitjnndjcndj",
+    fullname:{
+        username:{          // nesting of object
+            name:"sumit"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.username)
+
+const obj1={1:"a",2:"e",3:"d"}
+const obj2={4:"f",5:"s",6:"l"}
+
+// add to object
+
+const obj3=Object.assign({},obj1,obj2)
+//console.log(obj3)
+
+const obj4={...obj1,...obj2}
+//console.log(obj4);
+
+
+const obj5=[
+    {
+        name:"sumit"
+    },
+    {
+        vill:"basant"
+    }
+]
+
+//console.log(obj5[1].vill)
+
+
+console.log(Object.keys(obj1));  // is return ik array hoga
+// aise hi hum value bhi print krwa sakte hai
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
