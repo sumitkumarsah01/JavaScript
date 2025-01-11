b332dc224f9cbfcb9458d6c302837128510a217f
sumit kumar sah <sumitkumar92365@gmail.com>
2025-01-11 13:53:50 +0530

    DAY 5

---
+++ 10_function.js
@@ -0,0 +1,70 @@
+// function
+
+function name(){
+    console.log("Sumit");
+}
+
+name();
+
+// function addTwonumber(number1, number2){
+//     console.log(number1+number2)
+    
+// }
+//const result=addTwonumber(2,3);
+//console.log(result)  // this function are give the undefined because function are not return the value
+
+function addTwonumber(number1, number2){
+    let result=(number1+number2)
+    return result;
+    
+}
+const result=addTwonumber(3,4);
+//console.log(result)
+
+
+function loginusermessage(username){
+    if(username===undefined){
+        //console.log("please enter the name ");
+        return
+    }
+    return `${username} just logged in`
+}
+//console.log(loginusermessage())
+
+
+function calculateCartPrize(...num1){ // rest Operator
+    return num1;
+}
+//console.log(calculateCartPrize(20,200,30,49)); // array return krega 
+
+function calculateCartPrize2(val1 ,val2,...num1){ // rest Operator
+    return num1;
+}
+//console.log(calculateCartPrize2(20,200,30,49));
+
+
+const user={
+    username:"hitesh",
+    price:199
+}
+
+function handleObject(getObject){
+    console.log(getObject.username)
+}
+//handleObject(user)
+
+
+const myNewarray=[200,300,400,500];
+function returnSecondvalue(getArray){
+    return getArray[2];
+}
+console.log(returnSecondvalue(myNewarray))
+
+
+
+
+
+
+
+
+
