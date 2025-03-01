3662645ce4aba98515e800c1acb9deed4ec10bea
Sumit Kumar Sah <sumitkumar92365gmail.com>
2025-03-01 16:05:11 +0530

    Promise Part 2

---
+++ PromisesPart2.js
@@ -0,0 +1,67 @@
+let ans1=new Promise( (resolve ,reject) =>{
+    if(false){
+        return resolve();
+    }
+    else{
+        return reject();
+    }
+})
+
+ans.then(function(){
+    console.log("Not error");
+})
+.catch(function(){
+    console.log("error");
+})
+
+
+
+
+let ans2=new Promise((resolve,reject)=>{
+    let n=Math.floor(Math.random()*10);
+
+    if(n<5){
+        return resolve();
+    }
+    else{
+        return reject();
+    }
+})
+
+ans2.then(function(){
+    console.log("ye 5 se chota hai")
+}).catch(function(){
+    console.log("5 se bada hai")
+})
+
+//***************************************************************************************/
+
+
+let ans =new Promise(function(resolve,reject){
+    return resolve("Sbase pahle ghar awwo ")
+})
+
+let p1=ans.then(function(data){
+    console.log(data);
+    return new Promise(function(resolve,reject){
+        return resolve("pahle Gate kholo phir ander aawo")
+    })
+
+})
+
+let p2=p1.then(function(data){
+    console.log(data);
+    return new Promise(function(resolve,reject){
+        return resolve("khana khawo")
+
+    })
+
+})
+p2.then(function(data){
+    console.log(data);
+})
+
+
+
+
+
